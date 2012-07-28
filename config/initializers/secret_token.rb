e8f6d1c441a60760c61517b117a0ede47bcebf3a
sangarji <suryadithya@gmail.com>
2012-07-28 23:26:24 +0530

    Modified README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thandora::Application.config.secret_token = 'f6725b1dc399fec0ea263cd61650dba8bdcd4912035c51a08fb7b995816dbe8208ce48e060d01bc02d21fe045e230b3d74fbf5b9b1df50c32fad058b2cb6d49f'
